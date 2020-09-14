9a11b5f0845720a224b9e7ce1d80074d0a35e0b7
bulga138 <57503100+bulga138@users.noreply.github.com>
2020-09-14 01:27:48 -0300

    Update larralde.js

--- larralde.js
+++ larralde.js
@@ -16,9 +16,6 @@ let reply_data = fs.readFileSync("./replies.txt").toString();
 // Take each reply in the text file and put them into an array
 let replies = reply_data.split("\n");
 
-// Configure the array used for random replies (without replies.txt)
-//let replies = ["reply 1", "reply 2", "reply 3"];
-
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
@@ -26,21 +23,13 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content.startsWith(`${prefix}ping`)) {
-		message.channel.send('Pong.');
-	} else if (message.content.startsWith(`${prefix}beep`)) {
-		message.channel.send('Boop.');
-	} else if (message.content.includes(`Larralde`)) {
+	if (message.content.includes(`Larralde`)) {
 // Declare random for replies
-		let random = Math.floor(Math.random() * replies.length);
+	let random = Math.floor(Math.random() * replies.length);
 		message.channel.send(replies[random]);
-	} else if (message.content === `${prefix}server`) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${prefix}user-info`) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	} else if (message.content.includes('larralde')) {
 		// Declare random for select the replies
-		let random = Math.floor(Math.random() * replies.length);
+	let random = Math.floor(Math.random() * replies.length);
 		message.channel.send(replies[random]);
 	}
 });
